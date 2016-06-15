69bdf38cad236c32b8177bced09f685cea6f6b97
Lee <s.lee.t@live.com>
2016-06-15 03:21:20 +0000

    Mobile functionality added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      @feed_items = Micropost.all.paginate(page: params[:page])
     end
   end
 
